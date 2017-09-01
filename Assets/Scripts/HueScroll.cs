78996fb430078fe3dc9e6e622ac160a2ea084d93
Peter Khlopenkov <peter.khlopenkov@shopify.com>
2017-09-01 02:27:27 -0400

    Aug 31

---
+++ Assets/Scripts/HueScroll.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HueScroll : MonoBehaviour {
+
+    private SpriteRenderer mySpriteRenderer;
+
+    // Use this for initialization
+    void Start() {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        mySpriteRenderer.color = new Color(1, 0, 0, 0.2f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (mySpriteRenderer.color.r >= 1) {
+            if (mySpriteRenderer.color.b > 0)
+                mySpriteRenderer.color = new Color(1, mySpriteRenderer.color.g, mySpriteRenderer.color.b - 0.01f, 0.2f);
+            else
+                mySpriteRenderer.color = new Color(1, mySpriteRenderer.color.g + 0.01f, 0, 0.2f);
+        }
+        if (mySpriteRenderer.color.g >= 1) {
+            if (mySpriteRenderer.color.r > 0) 
+                mySpriteRenderer.color = new Color(mySpriteRenderer.color.r - 0.01f, mySpriteRenderer.color.g, mySpriteRenderer.color.b, 0.2f);          
+            else
+                mySpriteRenderer.color = new Color(mySpriteRenderer.color.r, mySpriteRenderer.color.g, mySpriteRenderer.color.b + 0.01f, 0.2f);
+        }
+        if (mySpriteRenderer.color.b >= 1) {
+            if (mySpriteRenderer.color.g > 0)
+                mySpriteRenderer.color = new Color(mySpriteRenderer.color.r, mySpriteRenderer.color.g - 0.01f, mySpriteRenderer.color.b, 0.2f);
+            else
+                mySpriteRenderer.color = new Color(mySpriteRenderer.color.r+0.01f, mySpriteRenderer.color.g, mySpriteRenderer.color.b, 0.2f);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,20 +12,16 @@ public class PlayerMovement : MonoBehaviour {
 
     private Vector3 playerScreenPosition;
     private Vector3 touchPosition;
-    private Vector3 teleportNewPosition;
     private int direction = 1;
     private int offset = 20;
-    private float teleportDistance = 10;
-    private bool walking = true;
-    private bool teleporting = false;
+
+    private float[] startTime = new float[2];
+    private float[] touchTime = new float[2];
+
+    private float tapMaxTime = 0.2f;
+
     private Touch[] touches;
-
-    public static bool moveTrigger;
-    public static bool teleportLeftTrigger;
-    public static bool teleportRightTrigger;
-    public static bool stopTrigger;
-    public static bool tapTrigger;
-
+    //
 
 
     // Use this for initialization
@@ -36,79 +32,58 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
         if (Input.touchCount > 0) {
+
+            //Direction Handling
             playerScreenPosition = cam.WorldToScreenPoint(transform.position);
-
             touchPosition = Input.GetTouch(0).position;
-
-            if (touchPosition.x > playerScreenPosition.x + offset && direction == -1) {
-                direction = 1; 
+            if (touchPosition.x > playerScreenPosition.x + offset) {
+                direction = 1;
             }
-            else if (touchPosition.x < playerScreenPosition.x - offset && direction == 1) {
+            else if (touchPosition.x < playerScreenPosition.x - offset) {
                 direction = -1;
-            }      
-        }
+            }
+            //
 
-        if (tapTrigger && transform.localScale.x == direction) {
-            flip();
-            tapTrigger = false;
-        }
+            touches = Input.touches;
 
-        if (!walking && animator.GetBool("walk") == true) {
-            float animationTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
-            if ( animationTime % 1 < 0.6f || 0.8f < animationTime % 1) {
-                animator.SetBool("walk", false);
+            //Touch Differentiation
+            for (int i = 0; i < Input.touchCount; i++) {
+
+
+                if (touches[i].phase == TouchPhase.Began) {
+                    startTime[i] = Time.time;
+                }
+
+                else if (touches[i].phase == TouchPhase.Ended || touches[i].phase == TouchPhase.Canceled) {
+                    if (touchTime[i] < tapMaxTime) {
+                        Vector3 mousePos = cam.ScreenToWorldPoint(touches[i].position);
+                        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+                        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+                        //if (hit.collider != null) {
+                        //}
+                    }
+                }
+                touchTime[i] = Time.time - startTime[i];
+
+                if (touchTime[i] > tapMaxTime) {
+                    rb.velocity = new Vector3 (maxSpeed*direction,0);                   
+                }
             }
         }
 
-        if (teleporting == true) {
-            transform.position = Vector3.Lerp(transform.position, teleportNewPosition, 1f);
-            if (transform.position.x - teleportNewPosition.x < 1 && transform.position.x - teleportNewPosition.x > -1)
-                teleporting = false;
+        if (rb.velocity.x > 5 || rb.velocity.x < -5) {
+            animator.SetBool("walk", true);
         }
         else {
-            if (moveTrigger)
-                playerMove();
-            if (stopTrigger)
-                playerStop();
-            if (teleportLeftTrigger)
-                playerTeleportLeft();
-            else if (teleportRightTrigger)
-                playerTeleportRight();
+            animator.SetBool("walk", false);
         }
-    }
 
-    private void playerMove() {
-        rb.velocity = new Vector2(direction * maxSpeed, rb.velocity.y);
-        animator.SetBool("walk", true);
-        walking = true;
-        if (transform.localScale.x == direction)
+        if (transform.localScale.x * direction < 0) {
             flip();
-    }
-
-    private void playerStop() {
-        rb.velocity = new Vector2(rb.velocity.x/2, rb.velocity.y);
-        walking = false;
-        stopTrigger = false;
-        moveTrigger = false;
-    }
-    
-    private void playerTeleportLeft() {
-        teleportNewPosition = new Vector3(transform.position.x - teleportDistance, transform.position.y, transform.position.z);
-        teleportLeftTrigger = false;
-        teleporting = true;
-        if (transform.localScale.x < 0)
-            flip();
-
-    }
-
-    private void playerTeleportRight() {
-        teleportNewPosition = new Vector3(transform.position.x + teleportDistance, transform.position.y, transform.position.z);
-        teleportRightTrigger = false;
-        teleporting = true;
-        if (transform.localScale.x > 0)
-            flip();
-
+        }    
     }
 
     private void flip() {
